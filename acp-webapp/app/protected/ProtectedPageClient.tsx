7bdebc489bc0d731d37986138c1b9efdf21fd313
zhiqilinn <zhiqilinn@gmail.com>
2025-04-30 00:20:54 -0400

    removed comments

--- acp-webapp/app/protected/ProtectedPageClient.tsx
+++ acp-webapp/app/protected/ProtectedPageClient.tsx
@@ -32,34 +32,6 @@ const ProtectedPageClient = () => {
     };
 
     return (
-        // <div>
-        //     <div className="flex min-h-screen flex-col">
-        //         {/* Header */}
-
-        //         <div className="flex flex-1">
-        //             {/* Sidebar */}
-        //             <div className="w-64 bg-gray-200">
-        //                 <div className="items-center justify-center bg-white flex p-4">
-        //                     <img
-        //                         alt="American Capital Partners Logo"
-        //                         src="https://placehold.co/120x40/FFFFFF/27ae60?text=ACP"
-        //                         className="h-10"
-        //                     />
-        //                 </div>
-        //                 <Sidebar setActiveComponent={setActiveComponent} />
-        //             </div>
-        //             {/* Header */}
-        //             <div className="flex-1 bg-white p-4 shadow-md">
-        //                 <Header />
-        //             </div>
-        //             {/* Main Content */}
-        //             <div className="flex-1 p-4">
-        //                 {renderActiveComponent()}
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <footer className="text-right text-sm text-gray-600 p-4">Powered by BlueSail AI</footer>
-        // </div>
         <div>
             <div className="flex min-h-screen">
             <div className="w-64 bg-gray-200">
